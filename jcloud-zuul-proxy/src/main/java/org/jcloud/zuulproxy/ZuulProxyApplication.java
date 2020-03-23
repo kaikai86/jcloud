14ca35aa4fd6ddd38a6978592dcb6adf6d7ef878
zhangkai <410618538@qq.com>
2020-03-23 15:00:47 +0800

    加入zuul路由并做简单配置

---
+++ jcloud-zuul-proxy/src/main/java/org/jcloud/zuulproxy/ZuulProxyApplication.java
@@ -0,0 +1,15 @@
+package org.jcloud.zuulproxy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyApplication.class);
+    }
+
+}
